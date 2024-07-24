41bb59ae5ea4303da21432cf56c7b876d0a7c329
thanasaksrisuwan <118355128+thanasaksrisuwan@users.noreply.github.com>
2024-07-24 19:32:29 +0700

    update packages

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/', function() {
+    return response()->json(['message' => 'Hello World']);
+});
+Route::get('/test', function() {
+    return response()->json(['message' => 'ok']);
+});
+Route::post('/login', [\App\Http\Controllers\LoginController::class, 'login']);
