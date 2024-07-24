cf40621efbc122de5822a2ece1bb079476257553
thanasaksrisuwan <118355128+thanasaksrisuwan@users.noreply.github.com>
2024-07-24 13:12:26 +0700

    init migrations

---
+++ backend/database/migrations/2024_07_23_110931_create_trips_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\Driver;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Driver::class);
+            $table->boolean('is_stared')->default(false);
+            $table->boolean('is_completed')->default(false);
+            $table->json('start_location')->nullable();
+            $table->json('end_location')->nullable();
+            $table->string('destination_name')->nullable();
+            $table->json('driver_location')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trips');
+    }
+};
